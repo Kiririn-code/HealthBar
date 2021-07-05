26501ad944ddfe5a2d02a24ebc6748c416599516
Kiririn <ya.aleks19982@yandex.ru>
2021-07-05 23:12:10 +0300

    Coroutine Added

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -11,19 +11,43 @@ public class HealthBar : MonoBehaviour
     [SerializeField] private Slider _slider;
     [SerializeField] private Player _player;
 
+    private Coroutine _StartChangeValue;
     private float _stepSize = 2f;
 
+    private void OnEnable()
+    {
+        _player.HealthChanged += RunCoroutine;
+    }
+
+    private void OnDisable()
+    {
+        _player.HealthChanged -= RunCoroutine;
+    }
 
     public void Start()
     {
-        _slider.maxValue = _player.CurrecntHealth;
+        _slider.maxValue = _player.MaxHelath;
         _slider.value = _slider.maxValue;
+        _image.color = _gradient.Evaluate(_slider.normalizedValue);
     }
 
-    public void Update()
+    private void RunCoroutine(int health)
     {
+        if (_StartChangeValue != null)
+            StopCoroutine(_StartChangeValue);
 
-        _slider.value = Mathf.Lerp(_slider.value, (float)_player.CurrecntHealth, _stepSize * Time.deltaTime);
-        _image.color = _gradient.Evaluate(_slider.normalizedValue);
+        _StartChangeValue = StartCoroutine(ChangeValue(health));
+    }
+
+    private IEnumerator ChangeValue(int health)
+    {
+        var UpdateFrameTime = new WaitForEndOfFrame();
+
+        while (_slider.value != health)
+        {
+            _slider.value = Mathf.Lerp(_slider.value, health, _stepSize * Time.deltaTime);
+            _image.color = _gradient.Evaluate(_slider.normalizedValue);
+            yield return UpdateFrameTime;
+        }
     }
 }
--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -5,24 +5,29 @@ using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private int MaxHealth;
-
+    [SerializeField] private int _maxHealth;
     private int _currentHealth;
 
+    public event UnityAction<int> HealthChanged;
+
+
     public int CurrecntHealth { get { return _currentHealth; } private set { _currentHealth = value; } }
+    public int MaxHelath { get { return _maxHealth; }}
 
     private void Awake()
     {
-        CurrecntHealth = MaxHealth;
+        CurrecntHealth = _maxHealth;
     }
 
     public void GetDamage(int damage)
     {
         CurrecntHealth -= damage;
+        HealthChanged?.Invoke(CurrecntHealth);
     }
 
     public void GetHealth(int health)
     {
         CurrecntHealth += health;
+        HealthChanged?.Invoke(CurrecntHealth);
     }
 }
